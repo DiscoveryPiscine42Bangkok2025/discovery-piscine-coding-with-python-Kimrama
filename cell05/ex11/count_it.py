33617faf78094c13125c15ef5f31c22c925e7b86
Kimrama <kimrama0x@gmail.com>
2025-09-16 14:55:32 +0700

    Enhance count_it.py to check for empty or whitespace-only parameters and provide appropriate messages

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import sys
+
 if len(sys.argv) < 2:
     print("none")
 else:
     print("parameter:", len(sys.argv) - 1)
     for i in sys.argv[1:]:
-        print(f"{i}: {len(i)}")
+        if i.strip() == "":
+            print(f'"{i}": empty or only spaces')
+        else:
+            print(f"{i}: {len(i)}")
