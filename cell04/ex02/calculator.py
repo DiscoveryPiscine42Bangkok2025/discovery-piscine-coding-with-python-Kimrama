5f52eb808940171059fa78da71862034f3b401dc
Kimrama <kimrama0x@gmail.com>
2025-09-16 13:42:04 +0700

    Refactor calculator.py to handle numeric input as floats and improve arithmetic operations

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 inp1 = input("Give me the first number: ")
 inp2 = input("Give me the second number: ")
+
+try:
+    num1 = float(inp1)
+    num2 = float(inp2)
+except ValueError:
+    print("Error: invalid input")
+    exit(1)
+
 print("Thank you!")
-print(f"{inp1} + {inp2} = {int(inp1) + int(inp2)}")
-print(f"{inp1} - {inp2} = {int(inp1) - int(inp2)}")
-print(f"{inp1} / {inp2} = {int(inp1) / int(inp2)}")
-print(f"{inp1} * {inp2} = {int(inp1) * int(inp2)}")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+if num2 == 0:
+    print("Division by 0 is not allowed.")
+else:
+    print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
