d9421330c979adde2cfdf8d676f7c6d8b1ae31bc
Kimrama <kimrama0x@gmail.com>
2025-09-16 11:09:06 +0700

    Refactor input prompts in i_got_that.py for clarity and consistency

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+user_input = input("What you gotta say? : ")
 while True:
-    user_input = input("What you gotta say? : ")
+    user_input = input("I got that! Anything else? : ")
     if user_input == "STOP":
         break
-    print("I got that! Anything else? : ", end="")
