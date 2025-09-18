f937d519a615efaa8bc24eff4c8ed8de89e054fb
Kimrama <kimrama0x@gmail.com>
2025-09-18 19:59:05 +0700

    Enhance count_it.py to include length of empty or whitespace-only parameters in output

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -6,7 +6,7 @@ if len(sys.argv) < 2:
 else:
     print("parameter:", len(sys.argv) - 1)
     for i in sys.argv[1:]:
-        if i.strip() == "":
-            print(f'"{i}": empty or only spaces')
+        if i.isspace() or i == "":
+            print(f'"{i}": empty or only spaces (len={len(i)})')
         else:
             print(f"{i}: {len(i)}")
